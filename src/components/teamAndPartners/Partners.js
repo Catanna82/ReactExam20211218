7f2596e696901db2b61abfec91c5699fbb4a0976
Catanna82 <anna820614@gmail.com>
2021-11-13 13:48:59 +0200

    Add PartnersCard component

--- src/components/teamAndPartners/Partners.js
+++ src/components/teamAndPartners/Partners.js
@@ -1,4 +1,5 @@
 import TeamCard from "./teamCard";
+import PartnersCard from "./PartnersCard";
 
 function Partners() {
     return (
@@ -14,27 +15,25 @@ function Partners() {
                 <div className="col-sm-6">
                     <div id="carousel-testimonials" className="carousel slide testimonails  wowload fadeInRight" data-ride="carousel">
                         <div className="carousel-inner">
-                            <div className="item active animated bounceInRight row">
-                                <div className="animated slideInLeft col-xs-2"><img alt="portfolio" src="images/team/1.jpg" width="100" className="img-circle img-responsive" /></div>
-                                <div className="col-xs-10">
-                                    <p> I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. </p>
-                                    <span>Angel Smith - <b>eshop Canada</b></span>
-                                </div>
-                            </div>
-                            <div className="item animated bounceInRight row">
-                                <div className="animated slideInLeft col-xs-2"><img alt="portfolio" src="images/team/2.jpg" width="100" className="img-circle img-responsive" /></div>
-                                <div className="col-xs-10">
-                                    <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
-                                    <span>John Partic - <b>Crazy Pixel</b></span>
-                                </div>
-                            </div>
-                            <div className="item  animated bounceInRight row">
-                                <div className="animated slideInLeft  col-xs-2"><img alt="portfolio" src="images/team/3.jpg" width="100" className="img-circle img-responsive" /></div>
-                                <div className="col-xs-10">
-                                    <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue.</p>
-                                    <span>Harris David - <b>Jet London</b></span>
-                                </div>
-                            </div>
+                            <PartnersCard
+                                active
+                                img="images/team/1.jpg"
+                                comments="I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
+                                username="Angel Smith"
+                                company="ebay"
+                            />
+                            <PartnersCard
+                                img="images/team/2.jpg"
+                                comments="I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
+                                username="John Doe"
+                                company="JDE Bulgaria"
+                            />
+                            <PartnersCard
+                                img="images/team/3.jpg"
+                                comments="I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
+                                username="Cammel Case"
+                                company="JS"
+                            />
                         </div>
                         <ol className="carousel-indicators">
                             <li data-target="#carousel-testimonials" data-slide-to="0" className="active"></li>
--- src/components/teamAndPartners/PartnersCard.js
+++ src/components/teamAndPartners/PartnersCard.js
@@ -1,12 +1,17 @@
 const PartnersCard = (props) => {
+    const className = [
+        'item',
+        props.active ? 'active' : '',
+        'animated bounceInRight row'
+    ].join(' ');
     return (
-        <>
+        <div className={className}>
             <div className="animated slideInLeft col-xs-2"><img alt="portfolio" src={props.img} width="100" className="img-circle img-responsive" /></div>
             <div className="col-xs-10">
                 <p> {props.comments} </p>
                 <span>{props.username} - <b>{props.company}</b></span>
             </div>
-        </>
+        </div>
     );
 }
 
